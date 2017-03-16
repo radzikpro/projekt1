341ee596e0e5f26a4133fca5e9a0b6be96d013cd
student <radboj10@gmail.com>
2017-03-16 18:44:10 +0100

    komentarz

--- app/src/main/java/com/bojko/radoslaw/apka/MainActivity.java
+++ app/src/main/java/com/bojko/radoslaw/apka/MainActivity.java
@@ -1,13 +1,29 @@
 package com.bojko.radoslaw.apka;
 
+import android.content.Intent;
 import android.graphics.Color;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView t1;
+
+
+    Button b1;
+    final static private int CAPTURE_IMAGE = 1;
+
+
 
 
     @Override
@@ -15,9 +31,58 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        t1=(TextView) findViewById(R.id.textView);
-        t1.setText("Kocham Alę");
-        t1.setTextColor(Color.RED);
+        b1=(Button)  findViewById(R.id.button);
+
+        b1.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                File mediaDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), "Apteka");
+                File imgFile = new File(mediaDir.getPath() + File.separator + "IMG_" + "RECEPTA_" + ".jpg");
+                Uri photoURI = FileProvider.getUriForFile(MainActivity.this, BuildConfig.APPLICATION_ID + ".provider", getOutputMediaFile());
+                Uri imgUri =Uri.fromFile(new File(imgFile.getAbsolutePath()));
+                Intent intent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE );
+                if (Build.VERSION.SDK_INT >= 24) {
+                    intent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                    startActivityForResult( intent, CAPTURE_IMAGE);
+                } else {
+                    intent.putExtra(MediaStore.EXTRA_OUTPUT, imgUri);
+                    startActivityForResult( intent, CAPTURE_IMAGE);
+                }
+
+            }
+        });
+    }
+    private static File getOutputMediaFile() {
+        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), "Apteka");
+        if (!mediaStorageDir.exists()) {
+            if (!mediaStorageDir.mkdirs()) {
+                Log.d("Apteka", "problem z utworzeniem katalogu");
+                return null;
+            }
+        }
+        // Create a media file name
+//        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss")
+//                .format(new Date());
+        File mediaFile;
+        mediaFile = new File(mediaStorageDir.getPath() + File.separator
+                + "IMG_" + "RECEPTA_" + ".jpg");
+        if (mediaFile.exists()) mediaFile.delete();
+        Log.d("Zdjecie1234567 : ", mediaFile.toString() );
+        return mediaFile;
+    }
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CAPTURE_IMAGE) {
+            if (resultCode == RESULT_OK) {
+                Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(i);
+            } else if (resultCode == RESULT_CANCELED) {
+                Log.d("Błąd", "Picture was not taken");
+            } else {
+                Log.d("Błąd", "Picture was not taken");
+            }
+        }
     }
 
     R.layout.activity_main
